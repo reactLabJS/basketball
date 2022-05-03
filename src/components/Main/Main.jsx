541ba2fdc8e1a957a4e1c34802f0633d6dd538f0
Aidarbek Umurzakov <aidarali.prod@gmail.com>
2022-05-03 22:56:01 +0600

    1

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Main.css'
+
+export const Main = () => {
+  return (
+    <div className='main'>
+        <div className="main-block">
+            
+        </div>
+        </div>
+  )
+}
